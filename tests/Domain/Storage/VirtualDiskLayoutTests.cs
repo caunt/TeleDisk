5fcde8acff2a826ed438a8a26bf1576d999d4918
Vitalii Kotenko <caunt.official@gmail.com>
2026-05-21 23:48:33 +0300

    test(tests): ✅ make test setup and layout checks more concise

--- tests/Domain/Storage/VirtualDiskLayoutTests.cs
+++ tests/Domain/Storage/VirtualDiskLayoutTests.cs
@@ -5,16 +5,12 @@ namespace TeleDisk.Tests.Domain.Storage;
 
 public sealed class VirtualDiskLayoutTests
 {
-    [Fact]
-    public void ChunkSize_ShouldBeFourMiB()
+    [Theory]
+    [InlineData(VirtualDiskLayout.ChunkSizeBytes, 4 * 1024 * 1024)]
+    [InlineData(VirtualDiskLayout.CapacityBytes, 1024L * 1024 * 1024)]
+    public void LayoutConstants_ShouldMatchExpectedValues(long actual, long expected)
     {
-        VirtualDiskLayout.ChunkSizeBytes.Should().Be(4 * 1024 * 1024);
-    }
-
-    [Fact]
-    public void Capacity_ShouldBeOneGiB()
-    {
-        VirtualDiskLayout.CapacityBytes.Should().Be(1024L * 1024 * 1024);
+        actual.Should().Be(expected);
     }
 
     [Fact]
